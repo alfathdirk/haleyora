37f26de7c3297cf54f5f045d5db7ed7e34ca548c
riodewanto <riodp.tech@gmail.com>
2024-10-13 00:01:21 +0700

    feat: exclude course from recommendation lists

--- apps/backoffice/app/(dashboard)/employees/[employeeId]/components/table/EmployeeCourseRecommendationTable.tsx
+++ apps/backoffice/app/(dashboard)/employees/[employeeId]/components/table/EmployeeCourseRecommendationTable.tsx
@@ -137,6 +137,7 @@ export const EmployeeCourseRecommendationTable = ({ employeeId }: { employeeId: 
         </div>
         <EmpCourseRecFormDialog
           employeeId={employeeId}
+          employeeRecommendations={data}
           triggerTitle={(
             <Button
                 className=""
