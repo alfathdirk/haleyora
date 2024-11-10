928f450537e98f5676156712c4e7613b79e87db1
riodewanto <riodp.tech@gmail.com>
2024-11-10 12:13:54 +0700

    feat: add dashboard filter

--- apps/backoffice/app/(dashboard)/page.tsx
+++ apps/backoffice/app/(dashboard)/page.tsx
@@ -1,17 +1,32 @@
 "use client";
 
+import { AuthContext } from "@/provider/Auth";
+import { useContext, useEffect, useState } from "react";
 import { CalendarDateRangePicker } from "@/components/date-range-picker";
 import { CourseOverview } from "@/components/CourseOverview";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { EmployeeOverview } from "@/components/EmployeeOverview";
-import { useContext } from "react";
-import { AuthContext } from "@/provider/Auth";
 import LatestCompletedQuiz from "@/components/LatestCompletedQuiz";
+import { useDirectusFetch } from "@/hooks/useDirectusFetch";
+import SelectFilterUnit from "@/components/SelectFilterUnit";
+import SelectFilterCourse from "@/components/SelectFilterCourse";
 
 export default function page() {
   const { currentUser } = useContext(AuthContext);
+  const fetch = useDirectusFetch();
+
+  const [selectedUnit, setSeletedUnit] = useState<string | null>(null);
+  const [selectedCourse, setSeletedCourse] = useState<string | null>(null);
+
+  const handleUnitChange = (unit: string | null) => {
+    setSeletedUnit(unit);
+  };
+
+  useEffect(() => {
+
+  }, []);
 
   return (
     <ScrollArea className="h-full">
@@ -20,10 +35,6 @@ export default function page() {
           <h2 className="text-3xl font-bold tracking-tight">
             Hi, {currentUser?.first_name} {currentUser?.last_name} 👋
           </h2>
-          <div className="items-center hidden space-x-2 md:flex">
-            {/* <CalendarDateRangePicker /> */}
-            {/* <Button>Download</Button> */}
-          </div>
         </div>
         <Tabs defaultValue="overview" className="space-y-4">
           <TabsList>
@@ -33,6 +44,20 @@ export default function page() {
             </TabsTrigger> */}
           </TabsList>
           <TabsContent value="overview" className="space-y-16">
+
+            {/* Filters */}
+            <div className="flex items-center justify-between px-1 space-x-2">
+              <div className="flex items-center w-full space-x-2 ">
+                <CalendarDateRangePicker />
+                <div className="w-1/3">
+                  <SelectFilterUnit selectedUnit={selectedUnit} onUnitChange={handleUnitChange} />
+                </div>
+                <div className="w-1/3">
+                  <SelectFilterCourse selectedCourse={selectedCourse} onCourseChange={setSeletedCourse} />
+                </div>
+              </div>
+            </div>
+
             <div className="grid grid-cols-1 gap-14 md:grid-cols-2 lg:grid-cols-7 min-h-96">
               <Card className="col-span-3">
                 <CardHeader>
@@ -55,6 +80,7 @@ export default function page() {
                 </CardContent>
               </Card>
             </div>
+
             <div>
               <LatestCompletedQuiz />
             </div>
