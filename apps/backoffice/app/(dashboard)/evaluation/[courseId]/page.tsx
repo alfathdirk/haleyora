8bfd34f7ae9ca5e32543c149ab0555e3651087db
riodewanto <riodp.tech@gmail.com>
2024-11-18 07:28:23 +0700

    feat: EvaluationCourseOverview

--- apps/backoffice/app/(dashboard)/evaluation/[courseId]/page.tsx
+++ apps/backoffice/app/(dashboard)/evaluation/[courseId]/page.tsx
@@ -5,10 +5,11 @@ import BreadCrumb from "@/components/breadcrumb";
 import { Heading } from "@/components/ui/heading";
 import { AuthContext } from "@/provider/Auth";
 import { useContext } from "react";
-// import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { useDirectusFetch } from "@/hooks/useDirectusFetch";
 import { EvaluationCourseTable } from "@/components/tables/EvaluationCourse/table";
 import { useParams, useSearchParams } from "next/navigation";
+import { EvaluationCourseOverview } from "@/components/EvaluationCourseOverview";
 
 export default function Page() {
   const { members, currentUser } = useContext(AuthContext);
@@ -16,6 +17,7 @@ export default function Page() {
   const { courseId } = useParams();
   const searchParams = useSearchParams();
 
+  const [data, setData] = useState<any[]>([]);
   const [totalCourseCompleted, setTotalCourseCompleted] = useState<number>(0);
   const [totalAvgQuizScore, setTotalAvgQuizScore] = useState<number>(0);
   const [totalAvgTaskScore, setTotalAvgTaskScore] = useState<number>(0);
@@ -27,76 +29,17 @@ export default function Page() {
     search: "",
   });
 
-  const fetchAverageScores = useCallback(async () => {
+  const fetchData = useCallback(async () => {
     try {
-      const filterParams: any = {};
-
-      if (filters.search) {
-        filterParams["full_name"] = { _contains: filters.search };
-      }
-      if (filters.unit_pln) {
-        filterParams["unit_pln"] = { _eq: filters.unit_pln };
-      }
-      if (!["Administrator", "Admin Pusat"].includes(currentUser?.role?.name ?? "")) {
-        filterParams["employee_id"] = { _in: members };
-      }
-
-      const { data: res } = await fetch.get("items/employee", {
-        params: {
-          fields: [
-            "id",
-            "employee_course.exam_score",
-            "employee_course.tasks_score",
-            "employee_course.completed",
-            "employee_course.course.is_open_task",
-            "employee_course.course.is_open_exam",
-          ],
-          filter: filterParams,
-        },
-      });
-
-      // Aggregation variables
-      let totalQuizCount = 0;
-      let totalExamScore = 0;
-      let totalTaskCount = 0;
-      let totalTaskScore = 0;
-      let completedCourseCount = 0;
-
-      // Iterate over each employee's courses to compute totals
-      res?.data?.forEach((employee: any) => {
-        employee.employee_course?.forEach((course: any) => {
-          if (course?.completed) {
-            completedCourseCount += 1;
-
-            if (course.course?.is_open_exam) {
-              totalQuizCount += 1;
-              totalExamScore += Number(course.exam_score ?? 0);
-            }
-
-            if (course.course?.is_open_task) {
-              totalTaskCount += 1;
-              totalTaskScore += Number(course.tasks_score ?? 0);
-            }
-          }
-        });
-      });
-
-      // Calculate averages
-      const averageExamScore = totalQuizCount > 0 ? totalExamScore / totalQuizCount : 0;
-      const averageTaskScore = totalTaskCount > 0 ? totalTaskScore / totalTaskCount : 0;
-
-      // Update state with calculated totals and averages
-      setTotalCourseCompleted(completedCourseCount);
-      setTotalAvgQuizScore(Number(averageExamScore.toFixed(1)) || 0);
-      setTotalAvgTaskScore(Number(averageTaskScore.toFixed(1)) || 0);
+      //
     } catch (error) {
       console.error("Error fetching average scores:", error);
     }
   }, [filters, currentUser, members, fetch]);
 
   useEffect(() => {
-    fetchAverageScores();
-  }, [fetchAverageScores]);
+    fetchData();
+  }, []);
 
   const handleFilterChange = (newFilters: {
     unit_pln: string | null;
@@ -108,50 +51,34 @@ export default function Page() {
   return (
     <>
       <div className="flex-1 space-y-2">
-        <BreadCrumb items={[{ title: "Evaluasi", link: "/evaluation" }, { title: searchParams.get("name") ?? "", link: "#" } ]} />
+        <BreadCrumb
+          items={[
+            { title: "Evaluasi", link: "/evaluation" },
+            { title: searchParams.get("name") ?? "", link: "#" },
+          ]}
+        />
 
         <div className="flex items-start justify-between !mb-10">
-          <Heading title={searchParams.get("name") ?? ""} description='' />
+          <Heading title={searchParams.get("name") ?? ""} description="" />
         </div>
 
-        {/* <div className="grid gap-4 !mb-10 md:grid-cols-2 lg:grid-cols-3">
-          <Card>
+        <div className="grid gap-4 !mb-10 md:grid-cols-2">
+          <Card className="col-span-1">
             <CardHeader className="flex flex-row items-center justify-between pb-2 space-y-0">
-              <CardTitle className="text-sm font-medium">
-                Pembelajaran Selesai
-              </CardTitle>
+
             </CardHeader>
-            <CardContent>
-              <div className="text-2xl font-bold">{totalCourseCompleted}</div>
+            <CardContent className="w-full h-72">
+              <EvaluationCourseOverview data={data} />
             </CardContent>
           </Card>
-          <Card>
-            <CardHeader className="flex flex-row items-center justify-between pb-2 space-y-0">
-              <CardTitle className="text-sm font-medium">
-                Nilai Rata - rata Ujian
-              </CardTitle>
-            </CardHeader>
-            <CardContent>
-              <div className="text-2xl font-bold">{totalAvgQuizScore}</div>
-            </CardContent>
-          </Card>
-          <Card>
-            <CardHeader className="flex flex-row items-center justify-between pb-2 space-y-0">
-              <CardTitle className="text-sm font-medium">
-                Nilai Rata - rata Tugas
-              </CardTitle>
-            </CardHeader>
-            <CardContent>
-              <div className="text-2xl font-bold">{totalAvgTaskScore}</div>
-            </CardContent>
-          </Card>
-        </div> */}
+        </div>
 
         <EvaluationCourseTable
           members={members}
           currentUser={currentUser}
           // onFilterChange={handleFilterChange}
           onFilterChange={() => {}}
+          onDataChange={(items: React.SetStateAction<any[]>) => setData(items)}
           courseId={courseId}
         />
       </div>
---
+++ apps/backoffice/components/EvaluationCourseOverview.tsx
@@ -0,0 +1,109 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { useDirectusFetch } from "@/hooks/useDirectusFetch";
+import { PieChart, Pie, Cell, ResponsiveContainer, Legend } from "recharts";
+import { DateRange } from "react-day-picker";
+
+interface Props {
+  data: Array<object>;
+}
+
+export function EvaluationCourseOverview({ data }: Props) {
+  const fetch = useDirectusFetch();
+  const [fetching, setFetching] = useState(true);
+  const [summary, setSummary] = useState<
+    Array<{ name: string; value: number }>
+  >([]);
+
+  const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042"];
+
+  async function convertData(data: Array<any>) {
+    setFetching(true);
+    try {
+      const lulus = data?.filter((item) => item?.passed === 'Ya')?.length;
+      const tidakLulus = data?.filter((item) => item?.passed !== 'Ya')?.length;
+
+      setSummary([
+        { name: "Lulus", value: lulus },
+        { name: "Tidak Lulus", value: tidakLulus },
+      ]);
+    } catch (error) {
+      console.error("Error fetching:", error);
+    } finally {
+      setFetching(false);
+    }
+  }
+
+  useEffect(() => {
+    convertData(data);
+  }, [data]);
+
+  const renderCustomizedLabel = ({
+    cx,
+    cy,
+    midAngle,
+    innerRadius,
+    outerRadius,
+    percent,
+  }: any) => {
+    const RADIAN = Math.PI / 180;
+    const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+    const x = cx + radius * Math.cos(-midAngle * RADIAN);
+    const y = cy + radius * Math.sin(-midAngle * RADIAN);
+
+    return (
+      <text
+        x={x}
+        y={y}
+        fill="white"
+        textAnchor={x > cx ? "start" : "end"}
+        dominantBaseline="central"
+      >
+        {`${(percent * 100).toFixed(0)}%`}
+      </text>
+    );
+  };
+
+  const renderLegend = (props: any) => {
+    const { payload } = props;
+    return (
+      <div className="absolute flex flex-col w-1/2 left-[65%] -top-16 gap-y-3">
+        {payload.map((entry: any, index: number) => (
+          <div
+            key={`item-${index}`}
+            className="inline-flex items-center gap-x-2"
+          >
+            <div
+              className={"w-3 h-3"}
+              style={{ background: COLORS[index] }}
+            ></div>
+            {entry.value}
+          </div>
+        ))}
+      </div>
+    );
+  };
+
+  return (
+    <ResponsiveContainer width="100%" height="100%" className={"relative"}>
+      <PieChart>
+        <Pie
+          data={summary}
+          cx="30%"
+          cy="50%"
+          labelLine={false}
+          label={renderCustomizedLabel}
+          outerRadius={120}
+          fill="#8884d8"
+          dataKey="value"
+        >
+          {summary.map((entry, index) => (
+            <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+          ))}
+        </Pie>
+        <Legend content={renderLegend} verticalAlign="middle" align="center" />
+      </PieChart>
+    </ResponsiveContainer>
+  );
+}
